de11ae47754c1b4c9d25935b5e62af234ca30e66
Aleksandra Liani <114131692+JuliaLiani@users.noreply.github.com>
2024-01-22 20:17:02 -0500

    files uploaded

---
+++ waves.js
@@ -0,0 +1,25 @@
+//Button for mobile version
+const button = document.querySelector("#myButton")
+button.addEventListener("click", function () {
+  document.querySelector("#player").play()
+  document.querySelector("#myVideo").play()
+})
+//MINUTES
+const timer = 5
+//SECONDS
+let amountTime = timer * 60
+function calculateTime() {
+  const countdown = document.querySelector("#countdown")
+  let minutes = Math.floor(amountTime / 60)
+  let seconds = amountTime % 60
+  countdown.textContent = `${minutes} : ${seconds}`
+  amountTime--
+  if (amountTime < 0) {
+    stopTimer()
+    amountTime = 0
+  }
+  function stopTimer() {
+    clearInterval(timerId)
+  }
+}
+let timerId = setInterval(calculateTime, 1000)
